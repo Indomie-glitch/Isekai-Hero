cc477ace9260d1356e1851fdf436ffb200250095
Indomie-glitch <darthaarnav@gmail.com>
2021-06-22 19:22:09 +0700

    Initial commit

---
+++ src/Gameinitiation/Resources.java
@@ -0,0 +1,29 @@
+package Gameinitiation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Resources {
+    private static Resources resources = null;
+    private Resources() {}
+    public static Resources getInstance() {
+        if (null == resources) {
+            resources = new Resources();
+        }
+        return resources;
+    }
+
+
+
+    //Characters
+    Collection<Characters> characters = new ArrayList<>();
+
+    public Collection<Characters> getCharacters(){
+        return characters;
+    }
+    public void addCharacter(List<Characters> characters)
+    {
+        this.characters.addAll(characters);
+    }
+}
---
+++ src/Gameinitiation/gameinitiation.java
@@ -0,0 +1,20 @@
+package Gameinitiation;
+
+import java.util.Scanner;
+
+public class gameinitiation {
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Welcome to Dungeon Explorer!!");
+        System.out.println("Welcome to Lorien, adventurer. Our town is surrounded by many monsters. We need your help to save this town from the evil monsters \nChoose to except press enter");
+        String start = enter.nextLine();
+        if (start.equals("")) {
+            //Start game
+            System.out.println("Start game");
+            Characters.start();
+        }
+            else{
+                System.out.println("Ending game");
+            }
+        }
+    }
