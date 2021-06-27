7fe4e1d40cc4b7a93cd09b51fa1f88b4693ae372
Indomie-glitch <darthaarnav@gmail.com>
2021-06-27 14:25:22 +0700

    Added a new class called new Character. Fixed the if statement variable scope by converting it to switch and placing everything inside the case. I also added a try and catch block so the player cannot type something else. I also added so that each race has different amount of stats and added more stats like level, xp, health, attack and weapon.

--- src/character/New_Character.java
+++ src/character/New_Character.java
@@ -9,28 +9,81 @@ import java.util.Scanner;
 public class New_Character {
 
     public static void start() {
-
+        boolean keepRunning = true;
         List<Character> character = new ArrayList<Character>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your Characters Name");
-        String name = scanner.nextLine();
-        System.out.println("Enter the race of your character \nThe options are \nElf \nHuman \nDwarf \nHobbit \nWizard");
-        String race = scanner.nextLine();
-        int level = 0;
-        int xp = 0;
-        String weapon = null;
-        int health = 10;
-        int attack = 10;
 
-        Character newcharacter = new Character(name, race, level, xp, weapon, health, attack);
-        character.add(newcharacter);
-        Resources.getInstance().addCharacter(character);
-        System.out.println(newcharacter);
-        //level
-        //XP
-        //Weapon
-        //Defence
-        //Health
-        //Attack
+        while (keepRunning) {
+            try {
+                System.out.println("Enter your Characters Name");
+                String name = scanner.nextLine();
+                System.out.println("Enter the race of your character \nThe options are \n1. Elf \n2. Human \n3. Dwarf \n4. Hobbit \n5. Wizard");
+                int selection = Integer.parseInt(scanner.next());
+                switch (selection) {
+                    case 1 -> {
+                        int health = 58;
+                        int attack = 73;
+                        String race = "Elf";
+                        String weapon = "Wooden bow";
+                        int level = 0;
+                        int xp = 0;
+                        Character newcharacter = new Character(name, race, level, xp, weapon, health, attack);
+                        character.add(newcharacter);
+                        Resources.getInstance().addCharacter(character);
+                        System.out.println(newcharacter);
+                    }
+                    case 2 -> {
+                        int health = 47;
+                        int attack = 68;
+                        String race = "Human";
+                        String weapon = "Wooden Sword";
+                        int level = 0;
+                        int xp = 0;
+                        Character newcharacter = new Character(name, race, level, xp, weapon, health, attack);
+                        character.add(newcharacter);
+                        Resources.getInstance().addCharacter(character);
+                        System.out.println(newcharacter);
+                    }
+                    case 3 -> {
+                        int health = 28;
+                        int attack = 62;
+                        String race = "Dwarf";
+                        String weapon = "Wooden Axe";
+                        int level = 0;
+                        int xp = 0;
+                        Character newcharacter = new Character(name, race, level, xp, weapon, health, attack);
+                        character.add(newcharacter);
+                        Resources.getInstance().addCharacter(character);
+                        System.out.println(newcharacter);
+                    }
+                    case 4 -> {
+                        int health = 15;
+                        int attack = 24;
+                        String race = "Hobbit";
+                        String weapon = "Hands";
+                        int level = 0;
+                        int xp = 0;
+                        Character newcharacter = new Character(name, race, level, xp, weapon, health, attack);
+                        character.add(newcharacter);
+                        Resources.getInstance().addCharacter(character);
+                        System.out.println(newcharacter);
+                    }
+                    case 5 -> {
+                        int health = 77;
+                        int attack = 89;
+                        String race = "Wizard";
+                        String weapon = "Wooden stick";
+                        int level = 0;
+                        int xp = 0;
+                        Character newcharacter = new Character(name, race, level, xp, weapon, health, attack);
+                        character.add(newcharacter);
+                        Resources.getInstance().addCharacter(character);
+                        System.out.println(newcharacter);
+                    }
+                }
+            } catch(Exception ex){
+                System.out.println("You must have pressed something incorrect \n try again");
+            }
+        }
     }
 }
